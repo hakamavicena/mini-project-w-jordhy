5bedaf9b4656667f5224a18deeba26f7c5d5c6c6
Kamhana <hakammustain@gmail.com>
2025-08-28 14:39:54 +0700

    feat: add essential components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import GetToKnow from "./get-to-know.tsx";
+import Reasoning from "./reasoning.tsx";
+import Activity from "./activity.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <GetToKnow />
+    <Reasoning />
+    <Activity />
+  </StrictMode>
+);
---
+++ src/reasoning.tsx
@@ -0,0 +1,15 @@
+export default function Reasoning() {
+  return (
+    <div className="flex flex-col gap-8 min-h-screen justify-center">
+      <h1>Why kak Alvin?</h1>
+      <div className="flex gap-8">
+        <div className="bg-white h-[180px] w-full rounded-md">
+          <p className="text-black">Hakam Avicena </p>
+        </div>
+        <div className="bg-white h-[180px] w-full rounded-md">
+          <p className="text-black">Brenanda Jordhy </p>
+        </div>
+      </div>
+    </div>
+  );
+}
