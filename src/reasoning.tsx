72808ff223e1602b0ea929ef9a77b22f4d5c3b11
Jordhy Branenda <ethgalleryin@gmail.com>
2025-08-29 17:39:10 +0700

    feat: lanjut design jir

--- src/reasoning.tsx
+++ src/reasoning.tsx
@@ -2,23 +2,25 @@ import "./Reasoning.css";
 
 export default function Reasoning() {
   return (
-    <section className="reasoning-container reasoning">
-      <h2 className="section-title">Why Kak Alvin?</h2>
+    <section className="reasoning">
+      <div className="reasoning-container">
+        <h2 className="reasoning-title">Why Kak Alvin?</h2>
 
-      <div className="reasoning-grid">
-        <div className="reasoning-card">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s.
-          </p>
-        </div>
+        <div className="reasoning-grid">
+          <div className="reasoning-card">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
+            </p>
+          </div>
 
-        <div className="reasoning-card">
-          <p>
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
-          </p>
+          <div className="reasoning-card">
+            <p>
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+          </div>
         </div>
       </div>
     </section>
